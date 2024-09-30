c36365c105ad04bb3950ed7f4cbe8daae9f584a8
Hong, GyeongUk <ghdruddnr1@gmail.com>
2024-09-30 17:46:14 +0900

    1

---
+++ gyeongwook/gyeongwook1/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NetflixLogo from "./component/netfilxLog";
+
+function App() {
+  return <NetflixLogo />;
+}
+
+export default App;
---
+++ gyeongwook/gyeongwook2/src/components/LoopSlider.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+import image01 from "../assets/champ01.jpg";
+import image02 from "../assets/champ02.jpg";
+import image03 from "../assets/champ03.jpg";
+import image04 from "../assets/champ04.jpg";
+
+const LoopSlider = () => {
+  return (
+    <div style={{ width: "80%", maxWidth: "800px" }}>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
+        navigation
+        pagination={{ clickable: true }}
+        modules={[Navigation, Pagination, Autoplay]}
+      >
+        <SwiperSlide>
+          <img
+            src={image01}
+            alt="Image 1"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={image02}
+            alt="Image 2"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={image03}
+            alt="Image 3"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src={image04}
+            alt="Image 4"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default LoopSlider;
